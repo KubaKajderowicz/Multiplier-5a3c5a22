0752ef5ff1e57f1d8472b23f854dcbfae50abd69
KubaKajderowicz <2115394@talnet.nl>
2019-09-15 21:37:16 +0200

    Uitwerking van de exercise

---
+++ multiplier.php
@@ -0,0 +1,15 @@
+<?php
+
+echo "Ik ga 2 getallen met elkaar vermenigvuldigen \n";
+
+echo "Eerste getal: \n";
+
+$firstval = readline();
+
+echo "Tweede getal: \n";
+
+$secondval = readline();
+
+echo "Uw resultaat is: ".$firstval * $secondval;
+
+
